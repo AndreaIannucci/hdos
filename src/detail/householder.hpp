e98de7a85971a1ed5e4c35e2cac6e7dadebeddc1
Andrea Iannucci <andreaiannucci2000@gmail.com>
2026-08-06 10:16:20 +0100

    Householder .hpp file

--- src/detail/householder.hpp
+++ src/detail/householder.hpp
@@ -4,4 +4,21 @@
 
 namespace hdos::detail{
 double householder_reflector(std::span<double> x);
+void  apply_householder_left(
+    std::span<double> A,
+    std::size_t rows,
+    std::size_t cols,
+    std::size_t start_row,
+    std::size_t start_col,
+    std::span<const double> packed,
+    double tau);
+
+void  apply_householder_right(
+    std::span<double> A,
+    std::size_t rows,
+    std::size_t cols,
+    std::size_t start_row,
+    std::size_t start_col,
+    std::span<const double> packed,
+    double tau);
 }
